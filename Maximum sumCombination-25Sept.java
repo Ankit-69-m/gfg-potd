37ee62aa41e7b4caa1a1bd7d52c9a8ee4d0502f8
Ankit Mourya <113794564+Ankit-69-m@users.noreply.github.com>
2023-09-25 23:57:27 +0530

    Add files via upload

---
+++ Maximum sumCombination-25Sept.java
@@ -0,0 +1,33 @@
+class Solution {
+    static List<Integer> maxCombinations(int N, int K, int A[], int B[]) {
+        // code here
+        PriorityQueue<Integer> p = new PriorityQueue<>();
+        List<Integer> List = new ArrayList<>();
+        
+        Arrays.sort(A);
+        Arrays.sort(B);
+        
+        for(int i=N-1; i>N-K-1; i--){
+            for(int j=N-1; j>N-K-1; j--){
+                
+                int sum = A[i] + B[j];
+                
+                if(p.size()<K) p.add(sum);
+                
+                else{
+                    if(p.peek()<sum){
+                        p.remove();
+                        p.add(sum);
+                    }
+                    else break;
+                }
+            }
+        }
+        
+        while (p.size()>0){
+            List.add(0, p.poll());
+        }
+        
+        return List;
+    }
+}
